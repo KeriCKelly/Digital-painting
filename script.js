e085db4e16f55ad55c85d08550e4cbc8fa18fe5b
KeriCKelly <102998143+KeriCKelly@users.noreply.github.com>
2022-06-01 12:24:54 -0700

    colors and animations
    changed colors
    fixed translation animation on stars

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ var two = new Two({
   autostart: true
 }).appendTo(document.body);
 
-var stars = _.map(_.range(5), function() {
+var stars = _.map(_.range(20), function() {
   var x = Math.random() * two.width;
   var y = Math.random() * two.height;
-  var star = two.makeStar(x, y, 60, 30);
+  var star = two.makeStar(x, y, 70, 50);
   star.velocity = Math.random() * 5 + 5;
-  star.fill = 'rgb(0, 200, 255)';
+  star.noStroke().fill = 'rgb('
+  + Math.floor(Math.random() * 210 + 100) + ','
+  + Math.floor(Math.random() * 100) + ','
+  + Math.floor(Math.random() * 320) + ')';
+ return star;;
   star.noStroke();
   
   return star;
@@ -22,15 +26,17 @@ two.bind('update', function() {
 
 function updateStar(star) {
   star.rotation -= star.velocity / 100;
+  star.translation.y -= star.velocity -8;
+  if (star.translation.y < 0) {
+    star.translation.y = two.width * 1.125;
+  }
+    star.translation.x -= star.velocity -8;
   if (star.translation.x < 0) {
     star.translation.x = two.width * 1.125;
   }
 }
 
-document.onmousemove = function(e) {
-  star.translation -= star.velocity / 100;
-  var star = e.clientX; 
-    var star = e.clientY; 
-  star.translation.x += (star.destination.x - star.translation.x) * 0.0625;
-  star.translation.y += (star.destination.y - star.translation.y) * 0.0625;
-}
+// two.bind('update', function() {
+//   var star = _.each(stars, updateStar);
+// star.translation.x -= star.velocity *1/8;
+// }).play();
